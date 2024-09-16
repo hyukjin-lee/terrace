382500b162e2779fc837eaaabdb50adcd584b412
hyukjin-lee <gogokow27@gmail.com>
2024-09-17 00:08:33 +0900

    test code fix

--- src/tech/domain/test/TechArticle.unit.test.ts
+++ src/tech/domain/test/TechArticle.unit.test.ts
@@ -4,6 +4,6 @@ describe("TechArticle", () => {
   test("createUri", () => {
     const date = "1970-01-01T00:00:00.000Z";
     const slug = "slug";
-    expect(TechArticle.createUri({date, slug})).toBe("/blog/1970/01/01/slug");
+    expect(TechArticle.createUri({date, slug})).toBe("/tech/1970/01/01/slug");
   });
 });
